63ec6ad2fa526820bc4468c2a75ce8028d25251d
xxria17 <xxrcy32@gmail.com>
2021-04-26 11:40:19 +0900

    json 파일 읽어오는 방식 변경

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const $listContent = document.getElementById('content');
 let jsonFile = './recent.json'
 
+getFile(jsonFile);
 
 //tab menu
 function openTab(event, tabName) {
@@ -18,6 +19,40 @@ function openTab(event, tabName) {
     }
     document.getElementById(tabName).style.display = "block";
     event.currentTarget.className += " active";
+    jsonFile = './' +  event.currentTarget.getAttribute("id") + '.json';
+    getFile(jsonFile);
 }
 
 
+function getFile(fileName) {
+    const data = new XMLHttpRequest();
+    data.open('GET', fileName);
+    data.send();
+    data.onload = function () {
+        let item = JSON.parse(data.responseText);
+        for (key in item) {
+            const $img = document.createElement('img');
+            $img.setAttribute("id", "img");
+            $img.height = 200;
+            $img.width = 200;
+            $img.src = item[key].img;
+
+            const $title = document.createElement('div');
+            $title.setAttribute("id", "title");
+            $title.innerText = item[key].title;
+
+            const $cp = document.createElement('div');
+            $cp.setAttribute("id", "cp");
+            $cp.innerText = item[key].cp;
+
+            const $container = document.createElement('div');
+            $container.setAttribute("id","item");
+            $container.appendChild($img);
+            $container.appendChild($title);
+            $container.appendChild($cp);
+
+
+            $listContent.appendChild($container);
+        }
+    }
+}
