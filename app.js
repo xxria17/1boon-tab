652e5eb3f776b94949c5ee0a7e91a3e9a9f5621d
xxria17 <xxrcy32@gmail.com>
2021-04-26 11:39:43 +0900

    탭 메뉴 구현 방식 변경

--- app.js
+++ app.js
@@ -1,70 +1,23 @@
-const $list = document.getElementById('list');
-const $moreBtn = document.querySelector('.btn');
-const files = ['./popular.json', './view.json', './recent.json'];
-var jsonFile = './popular.json';
-// tab menu 선택
+const $listContent = document.getElementById('content');
+let jsonFile = './recent.json'
 
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.tabTarget)
-        console.log(target);
-        tabContents.forEach(tabContent => {
-            tabContent.classList.remove('active')
-        });
-        tabs.forEach(tab => {
-            tab.classList.remove('active')
-        });
-        target.classList.add('active')
-        tab.classList.add('active')
-    })
-})
-
-// json 파일 읽어오기
-var data = new XMLHttpRequest();
-data.open('GET', jsonFile);
-data.send();
-data.onload = function () {
-    var popular = JSON.parse(data.responseText);
-    for (key in popular) {
-
-        loadData(popular, key);
-
-        if ($list.childElementCount % 10 == 0) {
-            break;
+//tab menu
+function openTab(event, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        while ($listContent.hasChildNodes()) {
+            $listContent.removeChild($listContent.firstChild);
         }
     }
-
-    console.log($list.childElementCount);
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
 }
 
-// 리스트의 각 아이템에 값 할당
-function loadData(popular, key) {
-    const $img = document.createElement('img');
-    $img.setAttribute("id","img");
-    $img.src = popular[key].img;
 
-    const $title = document.createElement('div');
-    $title.setAttribute("id","title");
-    $title.innerText = popular[key].title;
-
-    const $url = document.createElement('a');
-    $url.setAttribute("href",popular[key].url);
-    // $url.innerText = popular[key].url;
-
-    const $cp = document.createElement('div');
-    $cp.setAttribute("id","cp");
-    $cp.innerText = popular[key].cp;
-
-    const $whole = document.createElement('div');
-    $whole.setAttribute("id","item");
-
-    $whole.append($img);
-    $whole.append($title);
-    // $whole.append($url);
-    $whole.append($cp);
-
-    $list.append($whole);
-}
